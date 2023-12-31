96e3479a46bac0acee703e0b59ad57675cb047d8
Damirchik @dmr4eg <fifth.dmr@gmail.com>
2023-12-31 02:58:50 +0100

    created a directory for tetris game project

---
+++ cmake-build-debug/CMakeFiles/tetrisgame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tetrisgame.dir/main.cpp.o"
+  "CMakeFiles/tetrisgame.dir/main.cpp.o.d"
+  "tetrisgame"
+  "tetrisgame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tetrisgame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
